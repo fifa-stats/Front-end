1af2f42555cd4b412d7dc6ac1690a04016c29c76
Jor_Poon <jormpoon@gmail.com>
2019-03-11 14:17:42 -0400

    login page in place and setting up privaterouter

---
+++ fifa-stats/src/components/Login.js
@@ -0,0 +1,67 @@
+//import the necessary dependencies 
+import React from 'react';
+import {connect} from 'react-redux';
+import {login} from '../actions';
+
+
+class Login extends React.Component {
+    //State to control input form for new and existing users
+    state = {
+        credentials: {
+            email: "",
+            password: ""
+        }
+    }
+    //function to type in username and password
+    handleChanges = e => {
+        this.setState({
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //function to log in to the application
+    userLogin = e => {
+        e.preventDefault();
+        //this.props.login();
+    }
+
+    render() {
+        return (
+            <div className="login-page">
+
+                <h1>FIFA STATS </h1>
+
+                <div className="login-img">
+                     {/* for a picture */}
+                 </div>
+
+                <div className="login-box">
+                    <h2>Log In Here</h2>
+                    <form onSubmit={this.userLogin}>
+                        <input 
+                        type="text"
+                        name="email"
+                        value={this.state.credentials.email}
+                        onChange={this.handleChange}
+                        placeholder="User Email"
+                        />
+                         <input 
+                          type="text"
+                          name="password"
+                          value={this.state.credentials.password}
+                          onChange={this.handleChange}
+                          placeholder="User Password"
+                        />
+                        <button className="login-btn">Log In</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    null,
+    {login}  
+)(Login);
---
+++ fifa-stats/src/components/PrivateRoute.js
@@ -0,0 +1,7 @@
+//import dependencies
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({componentt = Component, ...rest }) => {
+    
+}
