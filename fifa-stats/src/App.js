e463e79ef9b034beb152568150cdcca7bd853e64
Clark <rclark010@gmail.com>
2019-03-12 09:37:26 -0600

    Resolving conflicts.

--- fifa-stats/src/App.js
+++ fifa-stats/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
+import Login from './components/Login';
 import PlayerView from './components/PlayerView/PlayerView';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>FIFA '19 Stats</h1>
+          <Login />
         </header>
         <Route path="/player/:id" component={PlayerView} />
       </div>
--- fifa-stats/src/index.js
+++ fifa-stats/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers'
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+       <App />
+    </Provider>,
     document.getElementById('root')
 );
